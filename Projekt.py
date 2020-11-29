51ccb3f967b2a1f1ad5bed75435dc5f72a477a0d
gesztesisz <55894983+gesztesisz@users.noreply.github.com>
2020-11-29 20:53:39 +0100

    v2.2

--- Projekt.py
+++ Projekt.py
@@ -2,10 +2,14 @@ import cv2
 thresh = 128
 
 #read image1
-original = cv2.imread('kartya.png', cv2.IMREAD_GRAYSCALE)
-img_binary = cv2.threshold(original, thresh, 255, cv2.THRESH_BINARY)[1]
+original = cv2.imread("kartya.png")
+szurke1= cv2.imread("kartya.png" , cv2.IMREAD_GRAYSCALE)
+szurke2 = cv2.threshold(szurke1, thresh, 255, cv2.THRESH_BINARY)[1]
 
 
-cv2.imshow("Original",img_binary)
+cv2.imshow("Szurkearnyalat",szurke2)
+#cv2.imshow("Original2",original)
+cv2.imshow("Eredeti",original)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
